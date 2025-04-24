46e5988de9a827f7c3c3529aafb9dc4bf6053233
Dineshd5 <dineshcivilians6@gmail.com>
2025-04-24 19:08:23 +0530

    Fix: update API endpoints localhost to use production URL vercel

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Spinner from "../components/Spinner";
 import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import { MdOutlineAddBox } from "react-icons/md";
 import BooksTable from "../components/home/BooksTable";
 import BooksCard from "../components/home/BooksCard";
 
@@ -16,7 +14,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("http://localhost:5555/books")
+      .get("https://mern-bookstore-qfek.onrender.com/books")
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
